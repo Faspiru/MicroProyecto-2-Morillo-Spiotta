c55be040c90114008ef64c93516ff942e4ef4f32
faspiru <fabrizio.spiotta@correo.unimet.edu.ve>
2023-06-09 00:54:20 -0400

    prueba 9

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
     <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
